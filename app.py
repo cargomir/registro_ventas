7741cf7d7357decaad187026b613cf50ae755fc2
Carlos Andrés González Miranda <cargomida@gmail.com>
2026-03-20 19:20:41 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -325,7 +325,7 @@ with st.expander("📊 Resumen de ventas"):
             f"${total_general:,.0f}".replace(",", ".")
         )
 
-with st.expander("💰 Consultar precios vigentes"):
+with st.expander("💰 Consultar precios"):
     df_precios_mostrar = pd.DataFrame({
         "Producto": [
             "Promoción completo + bebida",
